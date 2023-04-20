e3a050dac790cb7ee6b0e2e3158d647c3a3604be
IzzyHuang <92131151+IzzyHuang@users.noreply.github.com>
2023-04-19 19:43:04 -0500

    finished step 3

--- build.rs
+++ build.rs
@@ -1,6 +1,9 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::configure()
         .build_server(true)
-        .compile(&["proto/publish.proto", "proto/consume.proto"], &["proto/"])?;
+        .compile(&["proto/consume.proto"], &["proto/"])?;
+    tonic_build::configure()
+        .build_server(true)
+        .compile(&["proto/publish.proto"], &["proto/"])?;
     Ok(())
 }
