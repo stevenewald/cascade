cf7c654ddd5683ec1d16c76a52735c9edb04e67e
stevenewald <stevenewald6@gmail.com>
2023-04-21 15:20:23 -0500

    Fixed service structure + added documentation

--- src/broker/main.rs
+++ src/broker/main.rs
@@ -7,19 +7,20 @@ mod publish {
 mod consume {
     tonic::include_proto!("consume");
 }
-use publish::publish_to_broker_server::{PublishToBroker, PublishToBrokerServer};
-use publish::{PublishDataToBroker, BrokerToPublisherAck};
 use consume::consume_from_broker_server::{ConsumeFromBroker, ConsumeFromBrokerServer};
-use consume::{ConsumeDataFromBroker, BrokerToConsumerAck};
+use consume::{BrokerToConsumerAck, ConsumeDataFromBroker, Pair};
+use publish::publish_to_broker_server::{PublishToBroker, PublishToBrokerServer};
+use publish::{BrokerToPublisherAck, PublishDataToBroker};
 
-use tonic::{transport::Server, Request, Response, Status};
 use chrono::{DateTime, NaiveDateTime, Utc};
 use prost_types::Timestamp;
+use tonic::{transport::Server, Request, Response, Status};
 
 //this function is just to print and parse the timestamp of the event we receive
 fn timestamp_to_string(timestamp: Option<Timestamp>) -> String {
     if let Some(ts) = timestamp {
-        if let Some(naive_datetime) = NaiveDateTime::from_timestamp_opt(ts.seconds, ts.nanos as u32) {
+        if let Some(naive_datetime) = NaiveDateTime::from_timestamp_opt(ts.seconds, ts.nanos as u32)
+        {
             let datetime: DateTime<Utc> = DateTime::from_utc(naive_datetime, Utc);
             datetime.to_rfc3339()
         } else {
@@ -43,10 +44,15 @@ impl PublishToBroker for BrokerServer {
         data_received: Request<PublishDataToBroker>,
     ) -> Result<Response<BrokerToPublisherAck>, Status> {
         // returning a response as BrokerToPublisherAck message as defined in .proto
-        println!("Received message: {}", data_received.get_ref().event_name);
+        println!("Received message from producer: {}", data_received.get_ref().event_name);
         Ok(Response::new(BrokerToPublisherAck {
             // reading data from request which is awrapper around our PublishDataToBroker message defined in .proto
-            response_to_producer: format!("Broker response: received event with name {} and timestamp {} and number {}", data_received.get_ref().event_name, timestamp_to_string(data_received.get_ref().timestamp.clone()), data_received.get_ref().number.to_string()),
+            response_to_producer: format!(
+                "successfully rx event with name {} and timestamp {} and number {}",
+                data_received.get_ref().event_name,
+                timestamp_to_string(data_received.get_ref().timestamp.clone()),
+                data_received.get_ref().number.to_string()
+            ),
         }))
     }
 }
@@ -60,26 +66,38 @@ impl ConsumeFromBroker for BrokerServer {
         data_received: Request<ConsumeDataFromBroker>,
     ) -> Result<Response<BrokerToConsumerAck>, Status> {
         // returning a response as BrokerToConsumerAck message as defined in .proto
-        println!("Received message: {}", data_received.get_ref().event_name);
+        println!(
+            "Received message from consumer: {}\nWith key number: {}",
+            data_received.get_ref().event_name,
+            data_received.get_ref().number.to_string()
+        );
+        let response_pair = Pair {
+            event_name: String::from("event 1"),
+            timestamp: Some(Timestamp {
+                seconds: chrono::Utc::now().timestamp(),
+                nanos: 0,
+            }),
+        };
         Ok(Response::new(BrokerToConsumerAck {
-            // reading data from request which is awrapper around our PublishDataToBroker message defined in .proto
-            pair: ["example string".to_string(), Utc::now()],
-            // pair: format!("Broker response: received event with name {} and timestamp {}", data_received.get_ref().event_name, timestamp_to_string(data_received.get_ref().timestamp.clone())),
+            pair_vec: vec![response_pair],
         }))
     }
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // defining address for our service
+    // defining address for our server
     let addr = "[::1]:50051".parse().unwrap();
-    // creating a service
-    let server = BrokerServer::default();
-    println!("Server listening on {}", addr);
-    // adding our service to our server.
+
+    // we have to define a service for each of our rpcs
+    //andrew, these will soon be the bane of your existence (concurrency)
+    let service1 = BrokerServer::default();
+    let service2 = BrokerServer::default();
+    println!("Server listening on port {}", addr);
+    // adding services to server and serving
     Server::builder()
-        .add_service(PublishToBrokerServer::new(server))
-        .add_service(ConsumeFromBrokerServer::new(server))
+        .add_service(PublishToBrokerServer::new(service1))
+        .add_service(ConsumeFromBrokerServer::new(service2))
         .serve(addr)
         .await?;
     Ok(())
