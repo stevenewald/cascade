27ff3c3987c7a8a021177ce8d68115e5c2c851e5
miyaliu627 <107294785+miyaliu627@users.noreply.github.com>
2023-05-12 17:12:45 -0500

    rough implementation of producer

--- src/producer/main.rs
+++ src/producer/main.rs
@@ -27,8 +27,10 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let coordinator_channel = tonic::transport::Channel::from_static("http://[::1]:50001")
         .connect()
         .await?;
+    
     // creating gRPC client for KafkaMetadataService from channel
     let mut kafka_metadata_service_client = KafkaMetadataServiceClient::new(coordinator_channel);
+    
     // creating a channel ie connection to server
     // let broker_channel = tonic::transport::Channel::from_static("http://[::1]:50051")
     //     .connect()
@@ -45,22 +47,64 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let num_partitions = metadata_response.brokers.len();
     println!("Received metadata from coordinator with {} partitions.", num_partitions);
 
+
+    //1. from received metadata, copy(clone()) the partitions
+    let partitions = metadata_response.brokers.clone(); //is there a function to access just the partition we want? or is cloning the whole response fine?
+    
+    //2. for each broker/partition (effectively same), establish a connection and put it in a vector
+    let mut clients = Vec::new();
+    for broker in &metadata_response.brokers {
+        //let address = format!("{}:{}", broker.host, broker.port);
+        let broker_channel = tonic::transport::Channel::from_shared("http://[::1]:5000")?
+            .connect()
+            .await?;
+        let client_connection_to_broker = PublishToBrokerClient::new(broker_channel);
+        clients.push(client_connection_to_broker); //is client and broker the same in this case? 
+    }
+
     // events_to_send (made up) = [a, b, c, d]
-    // brokers = [1, 2, 3, 4]
+    // brokers = a vector from metadata, ie.: [1, 2, 3, 4]
     // counter = 0
     // while counter < len(events_to_send):
         // brokers[counter % len(brokers)].send(events_to_send[counter])
         // counter+=1
-
-
-
-
-    //1. from received metadata, copy(clone()) the partitions
-    //2. for each broker/partition (effectively same), establish a connection and put it in a vector
     //3. use the above pseudocode to loop through as the pseudocode implies
-
+    let events_to_send = vec!["a", "b", "c", "d"];
+    let brokers = metadata_response.brokers;
     let mut partition_index = 0;
 
+    while partition_index < events_to_send.len() {
+        // //approach 1
+        // let partition = partition_index % brokers.len();
+        // let broker = brokers[partition];
+
+        // //approach 2 that i think makes more sense?
+        // get the client connection for the next partition using round-robin
+        let mut client = clients[partition_index % clients.len()].clone(); //do i need to specify the broker still 
+                                                                        //or does client takes care of that when i'm doing client_connection_to_broker below?
+        // get the partition metadata for the next partition
+        let partition_metadata = partitions[partition_index % partitions.len()].clone();
+        
+        // // converting Duration to Timestamp
+        let now = SystemTime::now().duration_since(UNIX_EPOCH).expect("Time went backwards");
+        let timestamp = Timestamp {
+        seconds: now.as_secs() as i64,
+        nanos: now.subsec_nanos() as i32,
+        };
+
+        // // creating a new Request to send to broker
+        let mut rng = rand::thread_rng();
+        let data_to_broker = tonic::Request::new(PublishDataToBroker {
+            event_name: events_to_send[partition_index].to_string(),
+            timestamp: Some(timestamp),
+            number: rng.gen::<i32>(), // this is where the cpu usage (%) will go, make it a float though
+        });
+        // // sending data_to_broker and waiting for response
+        let ack_from_broker = client.send(data_to_broker).await?.into_inner();
+        println!("Received acknowledgement from broker with message: {}", ack_from_broker.response_to_producer);
+
+        partition_index += 1;
+    }
 
     // let now = SystemTime::now().duration_since(UNIX_EPOCH).expect("Time went backwards");
 
